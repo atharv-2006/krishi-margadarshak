d2f87eb28a9d0722b04843ed05b54ba2ff2fd169
atharv-2006 <atharvdeshmukh08@gmail.com>
2026-05-13 16:19:11 +0530

    Prepared for Render deployment

--- app.py
+++ app.py
@@ -848,5 +848,6 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
